01aabb34e581b337ba4a54199c40fb01437ddd8d
Nonamezzz322 <nonamezzz322@gmail.com>
2021-04-29 21:04:39 +0300

    Updates

---
+++ precache-manifest.0216aefefb7a714dbe659441dc9ce3cb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aebfaf2d3b82486c4dcb18778c30020b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "d4614f7212af689ac60c",
+    "url": "/resume/static/css/main.54c30d21.chunk.css"
+  },
+  {
+    "revision": "4abf042624d4c0eac5b0",
+    "url": "/resume/static/js/2.f5a8d259.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/resume/static/js/2.f5a8d259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4614f7212af689ac60c",
+    "url": "/resume/static/js/main.0b1f3040.chunk.js"
+  },
+  {
+    "revision": "2b17229f8e36d952a251",
+    "url": "/resume/static/js/runtime-main.6275bf14.js"
+  },
+  {
+    "revision": "43a3fe8d01c49f6d0a8e0d1095396df8",
+    "url": "/resume/static/media/Clarkson demo.43a3fe8d.ttf"
+  },
+  {
+    "revision": "2df057501d982273fa5b311b928f5a9c",
+    "url": "/resume/static/media/avatar.2df05750.jpg"
+  },
+  {
+    "revision": "b380bb2d37309b9d96ef606363882ff3",
+    "url": "/resume/static/media/icons8-github.b380bb2d.svg"
+  },
+  {
+    "revision": "07fc2ab352952de3d5c32b0bdcb74b91",
+    "url": "/resume/static/media/icons8-linkedin.07fc2ab3.svg"
+  },
+  {
+    "revision": "76bae8ba2bc913ca4b574710dabafd93",
+    "url": "/resume/static/media/javascript.76bae8ba.svg"
+  },
+  {
+    "revision": "c8782900400ca6ca518e274bdb101760",
+    "url": "/resume/static/media/jest.c8782900.png"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/resume/static/media/materialUI.d2b1e190.png"
+  },
+  {
+    "revision": "6aba3028ed1ac3b2bd1003d8fc3e9463",
+    "url": "/resume/static/media/pin.6aba3028.png"
+  },
+  {
+    "revision": "ac138987090c5f4fd14817d57dc1b71f",
+    "url": "/resume/static/media/typescript.ac138987.svg"
+  }
+]);
